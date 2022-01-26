66a665bb59ef501b36f0f55875410c50ce5cf38e
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-01-26 10:44:36 -0300

    fix(app): color counter component - remove quot to fontSize

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -5,7 +5,7 @@ const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   
   return (
     <View style={{ marginBottom: 5, marginTop: 20 }}>
-      <Text style={{ textAlign: 'center', fontSize: '16', fontWeight: '600' }}>{color}</Text>
+      <Text style={{ textAlign: 'center', fontSize: 16, fontWeight: '600' }}>{color}</Text>
       <Button onPress={() => onIncrease()} title={`Increase ${color}`} />
       <Button onPress={() => onDecrease()} title={`Decrease ${color}`} />
     </View>
